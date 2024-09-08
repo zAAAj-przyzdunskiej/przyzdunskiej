31e34646e2fedb1a69c18496aa2093fdfcfde0a3
saurosat <tuan.nguyen.1110@gmail.com>
2024-09-08 19:41:58 +0700

    added rolling spin to Login button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -86,10 +86,16 @@
 						<input type="password" id="password" name="password" required class="peer block min-h-[auto] w-full rounded border-2 bg-transparent px-3 py-[0.32rem] text-gray-900 dark:text-white" />
 					</div>
 					<!-- Submit button -->
-					<button
+					<button on:click={()=>isLoading.set(true)}
 						type="submit" 
 						class="hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-						Zaloguj się 
+
+						{#if $isLoading}
+						<div role="status" class="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+						<span>Ładowanie...</span>
+						{:else}
+						<span>Zaloguj się</span>
+						{/if}
 					</button>
 					<div class="relative mb-6">
 						{#if form?.issues}
