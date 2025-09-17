27901839eb8bd4ce3688cf24244b1223b62bf73f
zAAAj-przyzdunskiej <34404@gumed.edu.pl>
2025-09-17 19:45:32 +0200

    Update +page.svelte

--- src/routes/password/+page.svelte
+++ src/routes/password/+page.svelte
@@ -1,5 +1,8 @@
 <br><br>
 
-<iframe src="https://www.cognitoforms.com/f/4fgzlYo0lEy1S0Smf47Pww/32" allow="payment" style="border:0;width:100%;" height="1082"></iframe>
-<script src="https://www.cognitoforms.com/f/iframe.js"></script><br>.
+<!-- Make sure you include the 'amp-iframe' extension JavaScript in the <head> -->
+<amp-iframe src="https://www.cognitoforms.com/f/4fgzlYo0lEy1S0Smf47Pww/32" layout="responsive" resizable frameborder="0" scrolling="yes" height="1082" width="800" sandbox="allow-scripts allow-same-origin allow-popups"><div overflow></div><div placeholder></div></amp-iframe>
+
+<br>.
+
 
