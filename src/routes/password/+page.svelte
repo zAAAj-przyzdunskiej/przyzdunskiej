5ab1163d32ecc574368fe508bbcc5d684ac7a2dd
zAAAj-przyzdunskiej <34404@gumed.edu.pl>
2025-09-17 19:13:12 +0200

    Update +page.svelte

--- src/routes/password/+page.svelte
+++ src/routes/password/+page.svelte
@@ -1,2 +1,3 @@
 <br><br>
-<script src="https://www.cognitoforms.com/f/seamless.js" data-key="4fgzlYo0lEy1S0Smf47Pww" data-form="32"></script><br>
+
+<script src="https://www.cognitoforms.com/f/seamless.js" data-key="4fgzlYo0lEy1S0Smf47Pww" data-form="32"></script><br>.
