8db8b2bde32b98a34c6be9934b1b359088ccbbac
zAAAj-przyzdunskiej <34404@gumed.edu.pl>
2025-09-17 19:56:54 +0200

    Update +page.svelte

--- src/routes/hpv/+page.svelte
+++ src/routes/hpv/+page.svelte
@@ -1,8 +1,6 @@
 <br><br>
 
-<iframe src="https://www.cognitoforms.com/f/4fgzlYo0lEy1S0Smf47Pww/32" allow="payment" style="border:0;width:100%;" height="1300"  
-				class="w-full h-full max-w-full border-none" scrolling="no"
-				on:load={() => window.parent.scrollTo(0,0)} allowtransparency={true} allow="geolocation; microphone; camera; fullscreen"></iframe>
+<iframe src="https://www.cognitoforms.com/f/4fgzlYo0lEy1S0Smf47Pww/32" allow="payment" style="border:0;width:100%;" height="1300"></iframe>
 <script src="https://www.cognitoforms.com/f/iframe.js"></script>
 
 <br>.
