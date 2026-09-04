bbd0ee8972de6a0036f24df8becec55c5a404ebc
zAAAj-przyzdunskiej <34404@gumed.edu.pl>
2026-09-04 11:54:27 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -72,7 +72,7 @@
 </div>
 
 <footer >
-<p style="text-align: center;" textrwdproperties="{&quot;rwdMode_1&quot;:{&quot;fontSize&quot;:&quot;&quot;,&quot;lineHeight&quot;:&quot;&quot;,&quot;textAlign&quot;:&quot;center&quot;,&quot;letterSpacing&quot;:&quot;&quot;}}"><a data-linkdata="{&quot;type&quot;:&quot;webwave.link.CustomLinkData&quot;,&quot;target&quot;:&quot;blank&quot;,&quot;customLinkType&quot;:&quot;absoluteURL&quot;,&quot;href&quot;:&quot;http://aaaa&quot;,&quot;urlType&quot;:&quot;nofollow&quot;}">Informacja dotycząca wycieku danych elektroniczenj dokumentacji medycznej MyDR z 1 kwartału 2024 roku</a></p>
+<p style="text-align: center;"><a href="http://aaaa">Informacja dotycząca wycieku danych elektronicznej dokumentacji medycznej MyDR z 1 kwartału 2024 roku</a></p>
 <br>
 <br>
 	<svg xmlns="http://www.w3.org/2000/svg" class="-mb-0.5 w-full h" viewBox="0 0 1367.743 181.155">
